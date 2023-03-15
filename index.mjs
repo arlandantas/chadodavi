ce5e1422594d4d3966dfcc4e82d5556e44dce90c
Arlan Dantas <arlanjose@gmail.com>
2023-03-15 09:18:45 -0300

    adjust: Confirmation message modal

--- index.mjs
+++ index.mjs
@@ -1,7 +1,7 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-app.js'
 // import { getAnalytics } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-analytics.js'
 // import { getAuth } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-auth.js'
-import { getFirestore, collection, addDoc, getDocs, doc, where, query } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js'
+import { getFirestore, collection, setDoc, getDocs, doc, where, query, Timestamp } from 'https://www.gstatic.com/firebasejs/9.17.2/firebase-firestore.js'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -18,11 +18,15 @@ const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const INVITE_TABLE_NAME = "convidados";
 const getInviteCollection = () => collection(db, INVITE_TABLE_NAME);
-const btValidate = document.querySelector("#bt-validate")
+const btValidate = document.querySelector("#bt-validate");
+const btConfirm = document.querySelector("#bt-confirm");
+const btConfirmAbsence = document.querySelector("#bt-confirm-absence");
+const txtCode = document.querySelector(`#txt-code`);
+const slctQty = document.querySelector("#slct-qty")
+let currentInvite = null;
 
 async function validateCode() {
-  const codeField = document.querySelector(`#txt-code`);
-  let code = codeField.value;
+  let code = `${txtCode.value}`.toLocaleLowerCase();
   try {
     const q = query(getInviteCollection(), where("code", "==", code))
     btValidate.disabled = true
@@ -30,29 +34,62 @@ async function validateCode() {
     if (docs.size === 0) {
       btValidate.disabled = false
       alert("Código inválido!")
-      return codeField.focus()
+      return txtCode.focus()
     }
     docs.forEach((entry) => {
       const doc = entry.data();
       console.log({doc});
       const hasMany = doc.qty > 1
-      const slct = document.querySelector("#slct-qty")
-      slct.innerHTML = ''
+      slctQty.innerHTML = ''
       for (let i = 0; i < doc.qty; i++) {
         const option = document.createElement('option')
         option.textContent = i+1
-        slct.appendChild(option)
+        slctQty.appendChild(option)
       }
-      slct.value = 1
+      slctQty.value = 1
+      document.querySelector("#nomes").textContent = doc.names
+      document.querySelector("#tamanho").textContent = doc.size
       document.querySelectorAll(".only-multiple").forEach(e => e.classList.toggle("hidden", !hasMany))
       document.querySelectorAll(".not-multiple").forEach(e => e.classList.toggle("hidden", hasMany))
       hideModal('invite-code-modal')
       showModal('confirm-decline-modal')
+      currentInvite = { id: entry.id, ...doc }
     })
-    btValidate.disabled = false
   } catch (e) {
-    console.error("Error adding document: ", e);
-    alert("Failed to save document");
+    console.error("Error getting document: ", e);
+    alert("Não foi possível carregar seus dados no momento :(");
   }
+  btValidate.disabled = false
 }
 btValidate.onclick = validateCode
+
+function updateCurrentDoc(qty) {
+  return setDoc(
+    doc(db, INVITE_TABLE_NAME, currentInvite.id),
+    {
+      confirmed: parseInt(qty),
+      confirmationDatetime: Timestamp.fromDate(new Date()),
+    },
+    { merge: true }
+  )
+}
+
+async function confirm(qty, button) {
+  try {
+    const isPresence = qty > 0
+    button.disabled = true
+    await updateCurrentDoc(qty)
+    document.querySelector("#confirm-type").textContent = isPresence ? 'Presença' : 'Ausência'
+    document.querySelector("#confirm-size").textContent = currentInvite.size
+    document.querySelectorAll(".only-presence").forEach(e => e.classList.toggle("hidden", !isPresence))
+    hideModal('confirm-decline-modal')
+    showModal('confirm-message-modal')
+  } catch (e) {
+    console.error("Error updating document: ", e);
+    alert("Não foi possível confirmar sua ausência no momento :(");
+  }
+  button.disabled = false
+}
+
+btConfirm.onclick = () => confirm(slctQty.value, btConfirm)
+btConfirmAbsence.onclick = () => confirm(0, btConfirmAbsence)
