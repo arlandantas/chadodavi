ba973d9ed1dd0658f8e05797a9d56a654ace7939
Arlan Dantas <arlanjose@gmail.com>
2023-03-14 23:31:12 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+function showModal(modalId) {
+    getModalBackdrop();
+    const modal = document.querySelector(`#${modalId}`);
+    modal.classList.remove('hidden');
+}
+
+function hideModal(modalId) {
+    const backdrop = getModalBackdrop();
+    const modal = document.querySelector(`#${modalId}`);
+    modal.classList.add('hidden');
+    backdrop.remove();
+}
+
+function getModalBackdrop() {
+    let backdrop = document.querySelector(`#modal-backdrop`);
+    if (backdrop) {
+        return backdrop;
+    }
+    backdrop = document.createElement('div');
+    backdrop.id = 'modal-backdrop';
+    backdrop.classList.value = 'fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity';
+    document.querySelector("body").appendChild(backdrop);
+    return backdrop;
+}
+
+function validateCode() {
+    let code = document.querySelector(`#txt-code`).value;
+    alert("Validating: "+code);
+}
+
+showModal('invite-code-modal')
